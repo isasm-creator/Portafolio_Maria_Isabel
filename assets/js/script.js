e29b57898dcb53cb1b29e4589f798165bc3d0389
Maria Isabel <isa.sm.cl@gmail.com>
2026-01-09 22:32:22 -0300

    Creación de arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,260 @@ cardLinks.forEach(function (link) {
     window.location.href = './detalle.html';
   });
 });
+
+//Array de lugares: contendra listado de ciudades(minimo 5)
+/*
+id, nombre del lugar, temperatura actual, estado actual, pronostico semanal (array de objetos)
+*/
+const lugares = [
+  {
+    id: 1,
+    nombre: 'Colbún',
+    tempActual: 27,
+    estadoActual: 'Soleado',
+    pronosticoSemanal: [
+      {
+        dia: 'Lunes',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Martes',
+        min: 16,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Miercoles',
+        min: 17,
+        max: 29,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Jueves',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Viernes',
+        min: 14,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Sabado',
+        min: 15,
+        max: 26,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Domingo',
+        min: 16,
+        max: 26,
+        estado: 'Soleado',
+      }
+    ]
+  },
+    {
+    id: 2,
+    nombre: 'Talca',
+    tempActual: 27,
+    estadoActual: 'Soleado',
+    pronosticoSemanal: [
+      {
+        dia: 'Lunes',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Martes',
+        min: 16,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Miercoles',
+        min: 17,
+        max: 29,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Jueves',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Viernes',
+        min: 14,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Sabado',
+        min: 15,
+        max: 26,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Domingo',
+        min: 16,
+        max: 26,
+        estado: 'Soleado',
+      }
+    ]
+  },
+      {
+    id: 3,
+    nombre: 'Curicó',
+    tempActual: 27,
+    estadoActual: 'Soleado',
+    pronosticoSemanal: [
+      {
+        dia: 'Lunes',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Martes',
+        min: 16,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Miercoles',
+        min: 17,
+        max: 29,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Jueves',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Viernes',
+        min: 14,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Sabado',
+        min: 15,
+        max: 26,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Domingo',
+        min: 16,
+        max: 26,
+        estado: 'Soleado',
+      }
+    ]
+  },
+        {
+    id: 4,
+    nombre: 'Rancagua',
+    tempActual: 27,
+    estadoActual: 'Soleado',
+    pronosticoSemanal: [
+      {
+        dia: 'Lunes',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Martes',
+        min: 16,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Miercoles',
+        min: 17,
+        max: 29,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Jueves',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Viernes',
+        min: 14,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Sabado',
+        min: 15,
+        max: 26,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Domingo',
+        min: 16,
+        max: 26,
+        estado: 'Soleado',
+      }
+    ]
+  },
+          {
+    id: 5,
+    nombre: 'Santiago',
+    tempActual: 27,
+    estadoActual: 'Soleado',
+    pronosticoSemanal: [
+      {
+        dia: 'Lunes',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Martes',
+        min: 16,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Miercoles',
+        min: 17,
+        max: 29,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Jueves',
+        min: 18,
+        max: 28,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Viernes',
+        min: 14,
+        max: 25,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Sabado',
+        min: 15,
+        max: 26,
+        estado: 'Soleado',
+      },
+      {
+        dia: 'Domingo',
+        min: 16,
+        max: 26,
+        estado: 'Soleado',
+      }
+    ]
+  },
+];
