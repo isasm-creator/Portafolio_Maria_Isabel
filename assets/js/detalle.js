097204bf9c961eb22ac93b0b5beac1dc076a25b1
Maria Isabel <isa.sm.cl@gmail.com>
2026-02-04 01:15:06 -0300

    refactor: refactoriza función para filtrar lugar en detalle.html y crea lugares.js

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -1,3 +1,6 @@
+import lugares from './lugares.js';
+
+// QuerySelectorAll selecciona todos los elementos. En este caso todos los que tienen clase nav-link
 var links = document.querySelectorAll('.nav-link');
 
 links.forEach(function (link) {
@@ -8,281 +11,70 @@ links.forEach(function (link) {
   }
 });
 
-//Array de lugares: contendra listado de ciudades(minimo 5)//Array de lugares: contendra listado de ciudades(minimo 5)
-/*
-id, nombre del lugar, temperatura actual, estado actual, pronostico semanal (array de objetos)
-*/
-const lugares = [
-  {
-    id: 1,
-    nombre: 'Colbún',
-    tempActual: 27,
-    estadoActual: 'Soleado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Martes',
-        min: 16,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miercoles',
-        min: 17,
-        max: 29,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Jueves',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 14,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Sabado',
-        min: 15,
-        max: 26,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 16,
-        max: 26,
-        estado: 'Soleado',
-      }
-    ]
-  },
-    {
-    id: 2,
-    nombre: 'Talca',
-    tempActual: 27,
-    estadoActual: 'Soleado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Martes',
-        min: 16,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 17,
-        max: 29,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Jueves',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 14,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Sábado',
-        min: 15,
-        max: 26,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 16,
-        max: 26,
-        estado: 'Soleado',
-      }
-    ]
-  },
-      {
-    id: 3,
-    nombre: 'Curicó',
-    tempActual: 27,
-    estadoActual: 'Soleado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Martes',
-        min: 16,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 17,
-        max: 29,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Jueves',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 14,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Sábado',
-        min: 15,
-        max: 26,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 16,
-        max: 26,
-        estado: 'Soleado',
-      },
-    
-    ]
-  },
-        {
-    id: 4,
-    nombre: 'Rancagua',
-    tempActual: 29,
-    estadoActual: 'Soleado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Martes',
-        min: 16,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 17,
-        max: 29,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Jueves',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 14,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Sábado',
-        min: 15,
-        max: 26,
-        estado: 'Soleado',  
-      },
-      {
-        dia: 'Domingo',
-        min: 16,
-        max: 26,
-        estado: 'Soleado',
-      }
-    ]
-  },
-          {
-    id: 5,
-    nombre: 'Santiago',
-    tempActual: 26,
-    estadoActual: 'Soleado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Martes',
-        min: 16,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 17,
-        max: 29,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Jueves',  
-        min: 18,
-        max: 28,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 14,
-        max: 25,
-        estado: 'Soleado',    
-      },
-      {
-        dia: 'Sábado',
-        min: 15,
-        max: 26,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 16,
-        max: 26,
-        estado: 'Soleado',
-      }
-    ]
-  },
-];
-//Iconos para los estados del clima
+// Para agregar ícono dinámicamente dependiendo del estado actual del clima
 const ICONOS = {
-  Soleado: 'bi-sun',
-  Nublado: 'bi-cloud',
-  Lluvioso: 'bi-cloud-rain',
-  'Parcialmente Nublado': 'bi-cloud-sun',
+  Soleado: 'bi-brightness-high',
+  Nublado: 'bi-cloudy-fill',
+  Lluvioso: 'bi-cloud-rain-heavy',
+  'Parcialmente nublado': 'bi-cloud-sun',
 };
-//obtener el id del lugar desde la URL
+
+// 1. Obtener ID enviado por parámetros de la URL
 const urlParams = new URLSearchParams(window.location.search);
 
-//Extraer el id de los parametros
+// Extraer id de los parametros
 const locationId = urlParams.get('id');
+// console.log(locationId);
 
-//Console.log(locationId); //para verificar que se obtiene el id correctamente
+// 2. Filtrar lugar del array a partir del ID
+const lugarEncontrado = () => {
+  const lugar = lugares.find((lugar) => {
+    console.log(`Buscando en array lugares el lugar con id: ${locationId}`);
+    return lugar.id == locationId;
+  });
+  return lugar;
+};
 
-// 2.filtrar el lugar del array lugares que coincida con el id obtenido de la URL
-const lugarEncontrado = lugares.find((lugar) => {
-    console.log('Buscando en array lugares el lugar con id: ${locationId');
-    return lugar.id === locationId;
-});
-console.log(lugarEncontrado); //para verificar que se encuentra el lugar correctamente
+// Ejecutar función para buscar lugar a través de su ID
+const ciudadActual = lugarEncontrado();
+
+// 3. Capturar contenedor de información del lugar
+const lugarContainer = document.getElementById('lugar');
+
+const mostrarLugar = () => {
+  const content = `
+          <div class="card mb-3">
+            <div class="row g-0">
+              <div
+                class="col-lg-4 d-flex justify-content-center align-items-center"
+              >
+                <i class="bi ${
+                  ICONOS[ciudadActual.estadoActual]
+                }" style="font-size: 90px"></i>
+              </div>
+              <div class="col-lg-8">
+                <div class="card-body">
+                  <h2 class="card-title">${ciudadActual.nombre}</h2>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${
+                      ciudadActual.estadoActual
+                    }</li>
+                    <li class="list-group-item">Temperatura: ${
+                      ciudadActual.tempActual
+                    }°C</li>
+                  </ul>
+                  <p class="card-text">
+                    <small class="text-body-secondary"
+                      >Última actualización hace 3 minutos</small
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+  `;
+
+  lugarContainer.innerHTML += content;
+};
+
+mostrarLugar();
--- assets/js/lugares.js
+++ assets/js/lugares.js
@@ -1,32 +1,6 @@
-// Manejo del DOM con JavaScript//
-//QuerySelectorAll selecciona todos los elementos. En este caso todos los que tienen clase nav-link//
+//array de objetos que contiene la informacion del clima de diferentes lugares
+// id, nombre del lugar, temperatura actual, estado actual, pronostico semanal (array de objetos)
 
-var links = document.querySelectorAll('.nav-link');
-
-links.forEach(function (link) {
-  if (link.href === window.location.href) {
-    link.classList.add('active');
-  } else {
-    link.classList.remove('active');
-  }
-});
-
-// Manejo de Evento//
-
-var cardLinks = document.querySelectorAll('.card-link');
-
-// console.log(cardLinks); //
-
-cardLinks.forEach(function (link) {
-  link.addEventListener('click', function () {
-    window.location.href = './detalle.html';
-  });
-});
-
-//Array de lugares: contendra listado de ciudades(minimo 5)
-/*
-id, nombre del lugar, temperatura actual, estado actual, pronostico semanal (array de objetos)
-*/
 const lugares = [
   {
     id: 1,
@@ -280,38 +254,4 @@ const lugares = [
     ]
   },
 ];
-//Iconos para los estados del clima
-const ICONOS = {
-  Soleado: 'bi-sun',
-  Nublado: 'bi-cloud',
-  Lluvioso: 'bi-cloud-rain',
-  'Parcialmente Nublado': 'bi-cloud-sun', // Agregar comillas para clave con espacio
-};
-
-//Mostrar lugares en el index
-const lugaresContainer = document.getElementById('lugares');
-
-const mostrarLugares = () => {
-  lugares.forEach((lugar) => {
-    const tarjeta = `
-    <div class="col ">
-                    <div class="card text-center">
-                        <i class="bi ${ICONOS[lugar.estadoActual]} card__icon card__icon--${lugar.estadoActual.toLowerCase()}"></i>
-                        <div class="card-body">
-                            <h5 class="card-title">${lugar.nombre}</h5>
-                            <p class="card-text">${lugar.tempActual}°C</p>
-                            <p class="card-text">${lugar.estadoActual}</p>
-                        </div>
-                        <div class="card-body">
-                            <a href="./detalle.html?id=${lugar.id}" class="card-link">Ver detalle</a>
-                        </div>
-                    </div>
-                </div>
-                `;
-    lugaresContainer.innerHTML += tarjeta;
-  });
-};
-mostrarLugares();
-//TODO: hacer sumas de temperaturas, promedios, mínimo y máximos
-
-//TODO: mostrar estadisticas de la semana
+export default lugares;
